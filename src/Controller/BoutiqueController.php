66c381260928c879ca0a1a447002a30cd622b06b
Benj <benbourgeois@hotmail.fr>
2022-08-26 11:46:15 +0200

    ajout tri liste vehicule

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\VehiculeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -29,9 +30,18 @@ class BoutiqueController extends AbstractController
     }
 
     #[Route('/vehicules', name: 'app_liste')]
-    public function liste(VehiculeRepository $repo): Response
+    public function liste(Request $request, VehiculeRepository $repo,): Response
     {
-        $vehicules = $repo->findBy([], ['date_enregistrement' => 'DESC']);
+        $tri = ($request->query->get('tri'));
+        if ($tri==null)
+        {
+            $vehicules = $repo->findBy([], ['date_enregistrement' => 'DESC']);
+        }
+        else
+        {
+            $tri = explode('-', $tri);
+            $vehicules = $repo->findBy([], [$tri[0] => $tri[1]]);
+        }
 
         return $this->render('boutique/liste.html.twig', [
             'tabVehicules' => $vehicules,
